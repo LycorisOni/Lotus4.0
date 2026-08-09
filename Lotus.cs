e330a4b19551718d909b76e3d1cb52a9a4dfa5d9
LycorisOni <165130248+LycorisOni@users.noreply.github.com>
2026-08-09 16:47:00 -0400

    Add files via upload

--- Lotus.cs
+++ Lotus.cs
@@ -21,7 +21,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Lotus";
     public override string Author { get; init; } = "LunnayalunaLotus";
     public override List<string>? Contributors { get; init; } = ["LycorisOni"];
-    public override SemanticVersioning.Version Version { get; init; } = new("1.7.3");
+    public override SemanticVersioning.Version Version { get; init; } = new("1.7.4");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; } = null;
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = new()
@@ -124,7 +124,8 @@ public class Oni(
         await wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
         await wttCommon.CustomQuestService.CreateCustomQuests(assembly);
         await wttCommon.CustomQuestZoneService.CreateCustomQuestZones(assembly);
-        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);    
+        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+        await wttCommon.CustomDialogueService.CreateCustomDialogues(assembly);
         await Task.CompletedTask;
     }
 }
